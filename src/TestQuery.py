bfedcf009fbb561f3741d107e8667d6676d7ad03
Chris <christopher@cabuu.de>
2022-12-20 11:20:55 +0100

    Updated for Gen 9

--- src/TestQuery.py
+++ src/TestQuery.py
@@ -13,14 +13,30 @@ def main():
     with open(DEX_FILE, "r", encoding="utf-8") as f:
         data = PokemonDataMap(*(PokemonData.from_json(json.loads(line)) for line in f.readlines() if line))
 
-    while True:
-        text = input("\nName? ")
-        if not text:
-            break
-        matches = data.name(text)
-        for m in matches:
-            num = m.dex_entries.get_dex_num(Dex.GEN_8_DLC2)
-            print(f"{m.name_id}: {num if num else 'None'} ({m.dex_entries.get_dex_num(Dex.NATIONAL)})")
+    grouping = dict()
+    for mon in data:
+        if Dex.GEN_9 in mon.dex_entries:
+            if len(mon.misc_info.ev_yield.yields) == 1:
+                stat = list(mon.misc_info.ev_yield.yields.keys())[0]
+                if stat in grouping:
+                    grouping[stat].append(mon)
+                else:
+                    grouping[stat] = [mon]
+
+    for stat, mons in grouping.items():
+        print(f"{stat}:\n-----------------")
+        for mon in sorted(mons, key=lambda m: sum(m.misc_info.ev_yield.yields.values()), reverse=True):
+            print(f"{mon.name_id} : {mon.misc_info.ev_yield}")
+        print()
+
+    # while True:
+    #     text = input("\nName? ")
+    #     if not text:
+    #         break
+    #     matches = data.name(text)
+    #     for m in matches:
+    #         num = m.dex_entries.get_dex_num(Dex.GEN_8_DLC2)
+    #         print(f"{m.name_id}: {num if num else 'None'} ({m.dex_entries.get_dex_num(Dex.NATIONAL)})")
         # print("\n".join(json.dumps(m.to_json(), indent=2, ensure_ascii=False) for m in matches))
 
 
