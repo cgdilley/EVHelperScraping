31f8377eb5815f8682e82b42b00335859d8c4dcf
Christopher Dilley <cgdilley@gmail.com>
2025-10-31 14:08:37 +0100

    fix

--- src/Utils.py
+++ src/Utils.py
@@ -9,7 +9,7 @@ IMG_REGEX = re.compile(r"(<(source|img)[^>]+)>")
 def parse_html(filename: str) -> Element:
 
     with open(filename, "r", encoding="utf-8") as f:
-        text = f.read().replace("<br>", "<br/>").replace("<hr>", "hr/>")
+        text = f.read().replace("<br>", "<br/>").replace("<hr>", "<hr/>")
         text = IMG_REGEX.sub("\1/>", text)
     parser = XHTMLParser(recover=True, huge_tree=True)
     return fromstring(text, parser=parser)
